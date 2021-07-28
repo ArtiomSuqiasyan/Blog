6e5c6382a57622691d7fea0d69d5e3f0df635c81
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-07-29 01:57:30 +0400

    feat: add flexbox

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,8 +1,19 @@
-import RecipeReviewCard from "../Posts/Posts"
+import { Box } from "@material-ui/core";
+import RecipeReviewCard from "../Posts/Posts";
 
 export default function Post() {
-  const posts = JSON.parse(localStorage.getItem("posts"))
-    return posts.map((post)=>{
-      return <RecipeReviewCard title={post.title} info={post.info} method={post.method} />  
-    })
+  const posts = JSON.parse(localStorage.getItem("posts"));
+  return (
+    <Box display="flex">
+      {posts.map((post) => {
+        return (
+          <RecipeReviewCard
+            title={post.title}
+            info={post.info}
+            method={post.method}
+          />
+        );
+      })}
+    </Box>
+  );
 }
