9d8cd6f80b1f599578b744e82ae2a6b61ec68bbf
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-07-28 23:57:16 +0400

    feat: add create post functional

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -5,7 +5,30 @@ import FormControl from "@material-ui/core/FormControl";
 import Button from "@material-ui/core/Button";
 
 export default class CreatePost extends React.Component {
+
+  state = {
+    title: "",
+    info: "",
+    method: "",
+  };
+
+  handleChange = (props) => (event) => {
+    this.setState({ ...props, [props]: event.target.value });
+  };
+
+  handleSubmit = () => {
+    const prevLocalStorage = JSON.parse(localStorage.getItem("posts"))
+    const currentPost = [...prevLocalStorage, [this.state.title, this.state.info, this.state.method]]
+    localStorage.setItem("posts", JSON.stringify(currentPost))
+    this.setState({
+      title: "",
+      info: "",
+      method: "",
+    })
+  };
+
   render() {
+    const emptyInputValues = this.state.title === "" || this.state.info === "" || this.state.method === ""
     return (
       <div>
         <h2>Create post</h2>
@@ -14,6 +37,8 @@ export default class CreatePost extends React.Component {
           <InputLabel>Title</InputLabel>
           <Input
             id="title"
+            value={this.state.title}
+            onChange={this.handleChange("title")} 
           />
         </FormControl>
 
@@ -21,6 +46,8 @@ export default class CreatePost extends React.Component {
           <InputLabel>Info</InputLabel>
           <Input
             id="info"
+            value={this.state.info}
+            onChange={this.handleChange("info")}
             multiline
             rows={2}
           />
@@ -30,6 +57,8 @@ export default class CreatePost extends React.Component {
           <InputLabel>Method</InputLabel>
           <Input
             id="method"
+            value={this.state.method}
+            onChange={this.handleChange("method")}
             multiline
             rows={6}
           />
@@ -40,6 +69,8 @@ export default class CreatePost extends React.Component {
           color="primary"
           size="large"
           fullWidth
+          onClick={this.handleSubmit}
+          disabled={emptyInputValues}
         >
           Create Post
         </Button>
