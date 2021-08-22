83a7f2b4a72ca3f4d757baa4999d12a8ba8e5c14
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2021-08-22 18:58:26 +0400

    refactoring: refactoring login/logout

--- src/Components/Header/LogIn/Login.jsx
+++ src/Components/Header/LogIn/Login.jsx
@@ -3,14 +3,14 @@ import Button from "@material-ui/core/Button";
 export default function LogIn({ width }) {
   return (
     <>
-      
-      {localStorage.getItem("currentUser") ? <Button href="/logout" variant="contained" color="primary">
-          LogOut
-        </Button> :
 
-      <Button href="/login" variant="contained" color="primary">
-        LogIn
-      </Button>}
+      {localStorage.getItem("currentUser") ? <Button href="/logout" variant="contained" color="primary">
+        LogOut
+      </Button> :
+
+        <Button href="/login" variant="contained" color="primary">
+          LogIn
+        </Button>}
     </>
   );
 }
